9957b1530a150d32396741e72a9dd5c78e2abcf0
Emil Petrovski <emilpetrovski25@gmail.com>
2026-05-18 00:04:43 +0200

    initial project setup - Spring Boot project, Maven dependencies, PostgreSQL database connection

---
+++ src/main/java/mk/petsitter/PetSitterApplication.java
@@ -0,0 +1,12 @@
+package mk.petsitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSitterApplication.class, args);
+    }
+}
